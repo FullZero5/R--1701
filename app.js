6918d7feab3ce5eacd0be6d7844e754952f3afa3
@FullZero <fullzero5@gmail.com>
2019-12-01 08:21:25 +0300

    use pattrn jquery plugin

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
-$(document).ready(function(){
-    var p = $('p');
-    p.each(function(){
-        var t = $(this).text().replace(/\s/g, unescape('%a0')); 
-        var o = '';
-        for(var i = 0; i< t.length; i++){o += '<span class="normal">' + t[i] + '</span>';}
-        $(this).html(o);
-    });
-    function drop(){
-     var s = $('span.normal');
-      if (s.length == 0){
-       return;
-    } 
-    s.eq(Math.floor(Math.random() * s.length)).addClass('fallen').removeClass('normal');
-	  window.setTimeout(drop, 60);
+/*!
+ * jQuery Cascade plugin boilerplate
+ * Original author: @fullzero5
+ * this joke plugin
+ * Licensed under the MIT license
+ */
+;(function($, window, document, undefined ){
+    let pluginName = 'Cascade',
+        defaults = {
+            el: $('p')
+        };
+    function Plugin( element, options ) {
+        this.element = element;    
+        this.options = $.extend( {}, defaults, options) ;    
+        this._defaults = defaults;
+        this._name = pluginName;    
+        this.init();
     }
-    drop();
+    Plugin.prototype.init = function () {
+        // Run Code
+        let t = $(this.element).text().replace(/\s/g, unescape('%a0')),
+            o = '';
+        for(let i = 0; i< t.length; i++) {
+            o += '<span class="normal">' + t[i] + '</span>';
+        }
+        $(this.element).html(o);
+        function drop(){
+            let s = $('span.normal');
+            if (s.length == 0) {
+                return;
+            }
+            s.eq(Math.floor(Math.random() * s.length)).addClass('fallen').removeClass('normal');
+            window.setTimeout(drop, 60);
+        }    
+        drop();
+    };
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName,
+                new Plugin( this, options ));
+            }
+        });
+    }
+  })(jQuery, window, document );
+
+$(document).ready(function(){
+  $("p").Cascade();
 });
